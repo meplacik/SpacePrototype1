4e1dadbc620f482634a9bbfa21a7bf1e10ee3130
Madeline E Placik <meplacik@gmail.com>
2019-04-14 21:02:29 -0400

    first commit

--- SpacePrototype1/ViewController.swift
+++ SpacePrototype1/ViewController.swift
@@ -8,8 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    
+    @IBOutlet weak var pickerView1: UIPickerView!
+    @IBOutlet weak var pickerView2: UIPickerView!
+    @IBOutlet weak var pickerView3: UIPickerView!
+    
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let hours = ["12:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00"]
+    let am_pm = ["AM","PM"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if (pickerView == pickerView1){
+            let titleRow = days[row]
+            return titleRow
+        }
+        else if (pickerView == pickerView2){
+            let titleRow = hours[row]
+            return titleRow
+        }
+        else if (pickerView == pickerView3){
+            let titleRow = am_pm[row]
+            return titleRow
+        }
+        return ""
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if(pickerView == pickerView1){
+            return days.count
+        }
+        else if(pickerView == pickerView2){
+            return hours.count
+        }
+        else if(pickerView == pickerView3){
+            return am_pm.count
+        }
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if(pickerView == pickerView1){
+            textField1.text = days[row]
+        }
+        else if(pickerView == pickerView2){
+            textField2.text = hours[row]
+        }
+        else if(pickerView == pickerView3){
+            textField3.text = am_pm[row]
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
